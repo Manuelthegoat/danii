6aa773c70a699efc808314746358e7dd62d73f2e
Your Name~manuel <manuelcreation247@gmail.com>
2023-04-29 15:21:58 +0100

    gg

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -25,10 +25,10 @@ const Hero = () => {
 					<div class="background_image backcov"  ></div>
 					<div class="home_container">
 						<div class="home_container_inner d-flex flex-column align-items-center justify-content-center">
-							<div class="home_content text-center">
+						<div class="home_content text-center">
 								<div class="home_subtitle">New single release</div>
-								<div class="home_title"><h1>Love is all around</h1></div>
-								<div class="home_link"><a href="#">Listen on Soundcloud</a></div>
+								<div class="home_title"><h1>Slow Down</h1></div>
+								<div class="home_link"><a href="#">Listen on All Streaming Platforms</a></div>
 							</div>
 						</div>
 					</div>
@@ -38,10 +38,10 @@ const Hero = () => {
 					<div class="background_image backcov"  ></div>
 					<div class="home_container">
 						<div class="home_container_inner d-flex flex-column align-items-center justify-content-center">
-							<div class="home_content text-center">
+						<div class="home_content text-center">
 								<div class="home_subtitle">New single release</div>
-								<div class="home_title"><h1>Love is all around</h1></div>
-								<div class="home_link"><a href="#">Listen on Soundcloud</a></div>
+								<div class="home_title"><h1>Slow Down</h1></div>
+								<div class="home_link"><a href="#">Listen on All Streaming Platforms</a></div>
 							</div>
 						</div>
 					</div>
